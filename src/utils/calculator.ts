772c0b6f5427731ff3678fbeeb6e062a568995be
Matt DeMartino <matt.s.demartino@gmail.com>
2025-09-10 16:15:13 -0400

    Create function for converting time to seconds

--- src/utils/calculator.ts
+++ src/utils/calculator.ts
@@ -3,3 +3,8 @@ interface CalcVariables {
   times: number[];
   rest: number;
 }
+
+export function timeToSeconds(time: string) {
+  const timeSides = time.split(":");
+  return Number(timeSides[0]) * 60 + Number(timeSides[1]);
+}
