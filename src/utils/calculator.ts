6caa577342ff29d15881d9ff70c2e688ef45a385
Matt DeMartino <matt.s.demartino@gmail.com>
2025-09-10 16:28:37 -0400

    Add comments

--- src/utils/calculator.ts
+++ src/utils/calculator.ts
@@ -1,9 +1,11 @@
+// Stores user inputs
 interface CalcVariables {
   rounds: number;
   times: number[];
   rest: number;
 }
 
+// Takes a time such as 1:20 and converts it to seconds
 export function timeToSeconds(time: string) {
   const timeSides = time.split(":");
   return Number(timeSides[0]) * 60 + Number(timeSides[1]);
