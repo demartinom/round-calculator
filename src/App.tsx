51f42aac31c0f7497b62297c1138d611d914230e
Matt DeMartino <matt.s.demartino@gmail.com>
2025-09-11 16:01:21 -0400

    Create input for setting the number of rounds

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from "react";
+import { RoundTime } from "./components/timeInput";
 import "./styles/App.css";
-
 function App() {
-  return <></>;
+  const [rounds, setRounds] = useState(1);
+  const [roundTimes, setRoundTimes] = useState<string[]>([]);
+
+  useEffect(() => {
+    setRoundTimes(Array(rounds).fill("00:00"));
+  }, [rounds]);
+
+  return (
+    <>
+      <input
+        type="number"
+        name="Rounds"
+        id="RoundNums"
+        onChange={(e) => {
+          setRounds(Number(e.target.value));
+        }}
+      />
+    </>
+  );
 }
 
 export default App;
