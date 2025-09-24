a42720cb82a8cc16f25d8276b3b4becf5c27e72c
Matt DeMartino <matt.s.demartino@gmail.com>
2025-09-24 13:13:17 -0400

    Create component for setting rest time. Default of 2 min

---
+++ src/components/RestTime.tsx
@@ -0,0 +1,18 @@
+import { InputMask } from "@react-input/mask";
+
+type RestTimeProps = {
+  rest: string;
+  updateRest: (newTime: string) => void;
+};
+
+export const RestTime = ({ rest, updateRest }: RestTimeProps) => {
+  return (
+    <InputMask
+      mask="__:__"
+      replacement={{ _: /\d/ }}
+      placeholder="mm:ss"
+      onChange={(e) => updateRest(e.target.value)}
+      value={rest}
+    />
+  );
+};
